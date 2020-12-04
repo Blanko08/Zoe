6ffa2e7174cb8f2403415c0866d9ba4a099a01f1
Blanko08 <sbherrera08@gmail.com>
2020-12-04 20:32:31 +0100

    v0.0.4 Codigo Optimizado

---
+++ funciones.py
@@ -0,0 +1,64 @@
+import datetime
+import pyttsx3
+import requests
+import speech_recognition as sr
+
+
+#Esta función hace hablar a Zoe
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 160)
+    engine.setProperty('voice', 'spanish')
+    engine.setProperty('volume', 1)
+    engine.say(text)
+    engine.runAndWait()
+
+#Aquí recogeremos el audio del usuario
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    said = ""
+    try:
+        said = r.recognize_google(audio, language='es-ES')
+        print("Has dicho: " + said)
+    except:
+        print("Error al recoger audio")
+    return said.lower()
+
+#Función de saludo al iniciar el programa
+def saludo():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 6 and hour < 12:
+        speak("Buenos días!")
+    elif hour >= 12 and hour < 21:
+        speak("Buenas tardes!")
+    else:
+        speak("Buenas noches!")
+
+#Aquí creamos una función para "despertar" a Zoe
+def despertar(text):
+    ready = ['ey zoe', 'hey zoe', 'ok zoe', 'oye zoe', 'okay zoe'] #Lista de palabras que van a despertar a Zoe
+
+    text = text.lower()
+
+    for phrase in ready:
+        if phrase in text:
+            return True
+
+    #Devuelve Falso si no encuentra la palabra que hace despertar a Zoe
+    return False
+
+def clima():
+    api_address = 'https://api.weatherbit.io/v2.0/current?key=ec9c825a578f4e9b8ce68b321d331b84&lang=es&city='
+    speak("Dime el nombre de la ciudad")
+    print("Escuchando...")
+    city = get_audio()
+
+    url = api_address + city
+    respuesta = requests.get(url)
+    clima = respuesta.json()
+
+    datos_clima = clima['data'][0]['weather']['description'] + " . La temperatura actualmente es de" + str(clima['data'][0]['temp']) + "grados"
+
+    speak(datos_clima)
--- main.py
+++ main.py
@@ -1,99 +1,36 @@
-import time
-import datetime
-import pyttsx3
-import speech_recognition as sr
-import requests
-import pyaudio
-
-#Esta función hace hablar a Zoe
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 160)
-    engine.setProperty('voice', 'spanish')
-    engine.setProperty('volume', 1)
-    engine.say(text)
-    engine.runAndWait()
-
-#Aquí recogeremos el audio del usuario
-def get_audio():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    said = ""
-    try:
-        said = r.recognize_google(audio, language='es-ES')
-        print("Has dicho: " + said)
-    except:
-        print("Error al recoger audio")
-    return said.lower()
-
-
-#Aquí creamos una función para "despertar" a Zoe
-def despertar(text):
-    ready = ['ey zoe', 'hey zoe', 'ok zoe', 'oye zoe'] #Lista de palabras que van a despertar a Zoe
-
-    text = text.lower()
-
-    for phrase in ready:
-        if phrase in text:
-            return True
-
-    #Devuelve Falso si no encuentra la palabra que hace despertar a Zoe
-    return False
-
-
-#Función de saludo al iniciar el programa
-def saludo():
-    hour = int(datetime.datetime.now().hour)
-    if hour >= 6 and hour < 12:
-        speak("Buenos días!")
-    elif hour >= 12 and hour < 21:
-        speak("Buenas tardes!")
-    else:
-        speak("Buenas noches!")
-
-
-def clima():
-    api_address = 'https://api.weatherbit.io/v2.0/current?key=ec9c825a578f4e9b8ce68b321d331b84&lang=es&city='
-    speak("Dime el nombre de la ciudad")
-    city = get_audio()
-
-    url = api_address + city
-    respuesta = requests.get(url)
-    clima = respuesta.json()
-
-    datos_clima = clima['data'][0]['weather']['description'] + " . La temperatura actualmente es de" + str(clima['data'][0]['temp']) + "grados"
-
-    speak(datos_clima)
+import funciones
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 
 
 while True:
     print("Escuchando...")
-    text = get_audio().lower()
+    text = funciones.get_audio().lower()
 
-    if despertar(text) == True:
-        saludo()
-        speak("¿En qué puedo ayudarte?")
+    if funciones.despertar(text) == True:
+        funciones.saludo()
+        funciones.speak("¿En qué puedo ayudarte?")
         print("Escuchando...")
-        text = get_audio()
+        text = funciones.get_audio()
 
         saludo_strs = ["hola", "saludos"]
         for phrase in saludo_strs:
             if phrase in text:
-                speak("¿Hola, como estás?")
+                funciones.speak("¿Hola, como estás?")
 
         nombre_strs = ["cómo te llamas", "cuál es tu nombre"]
         for phrase in nombre_strs:
             if phrase in text:
-                speak("Me llamo Zoe")
+                funciones.speak("Me llamo Zoe")
 
         clima_strs = ["dime el tiempo", "dime el clima"]
         for phrase in clima_strs:
             if phrase in text:
-                clima()
+                funciones.clima()
 
         exit_strs = ["apágate zoe", "adiós zoe"]
         for phrase in exit_strs:
             if phrase in text:
-                speak("Hasta luego")
+                funciones.speak("Hasta luego")
                 break
